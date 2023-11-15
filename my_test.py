c5a616300c3cc969a386503ded61a2519d58aad0
unknown <zxm289263685@outlook.com>
2023-11-15 15:23:12 +0800

    update

--- my_test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-import cv2
-
-img_name = 'test_green.jpg'
-img1 = cv2.imdecode(np.fromfile(img_name, dtype=np.uint8), 1) ############
-img2 = cv2.imread(img_name)
-print(img1)
-print(img2)
--- run.sh
+++
@@ -1,1 +0,0 @@
-python train.py lpr.yml
